a2b421f2b648ece52faee92015ceb4a4062a0120
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2025-07-26 01:09:54 +0200

    Add math package and integrate backend API with web and widget apps

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,19 @@
+"use client"
+
 import { Button } from "@workspace/ui/components/button"
+import { api } from "@workspace/backend/convex/_generated/api";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Page() {
+  const data = useQuery(api.functions.getMany);
+  const add = useMutation(api.functions.add);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">apps/web</h1>
+        <Button size="sm" onClick={() => add({})}>Add</Button>
+        <div className="text-sm max-w-md w-full">{JSON.stringify(data, null, 2)}</div>
       </div>
     </div>
   )
---
+++ packages/backend/convex/functions.ts
@@ -0,0 +1,20 @@
+import { mutation, query } from "./_generated/server";
+
+export const getMany = query(async (ctx) => {
+  const users = await ctx.db.query("users").collect();
+  return users;
+});
+
+export const add = mutation({
+  args: {},
+  handler: async (ctx, args) => {
+    const user = await ctx.db.insert("users", {
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "password",
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    });
+    return user;
+  }
+})
